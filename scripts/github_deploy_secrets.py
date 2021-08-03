e32f2fe7a0fd3e7b025a839763abe65cbed47dee
danackerson <dan@ackerson.de>
2021-08-03 11:57:30 +0200

    refactored to use github module for secret updates

--- scripts/github_deploy_secrets.py
+++ scripts/github_deploy_secrets.py
@@ -1,92 +1,19 @@
 #!/usr/bin/env python3
-from getpass import getpass
-from io import StringIO
-from nacl import encoding, public
-from paramiko import RSAKey, ssh_exception
-from pathlib import Path
 from requests.exceptions import HTTPError
-from time import time
+from types import SimpleNamespace
 
-import base64
-import jwt
 import os
 import requests
-import sys
 
-GITHUB_APP_CLIENT_ID = os.environ['GITHUB_APP_CLIENT_ID']
-GITHUB_INSTALL_ID = os.environ['GITHUB_INSTALL_ID']
-GITHUB_SECRETS_PK_PEM_FILE = os.environ['GITHUB_SECRETS_PK_PEM_FILE']
+import github
+
 SSH_CERT_FILE = os.environ['SSH_CERT_FILE']
 SSH_PRIV_KEY = os.environ['SSH_PRIV_KEY']
 SSH_PUB_KEY = os.environ['SSH_PUB_KEY']
 ACME_JSON = "/home/ubuntu/traefik/acme.json"
 
 
-def fatal(message):
-    print('Error: {}'.format(message), file=sys.stderr)
-    sys.exit(1)
-
-
-# Token Exchange requires a JWT in the Auth Bearer header with this format
-def generate_id_token(iss, private_key, expire_seconds=600):
-    new_jwt = jwt.encode(
-        {'iss': iss, 'iat': int(time()), 'exp': int(time()) + expire_seconds},
-        private_key, algorithm='RS256')
-    # python3 jwt returns bytes, so we need to decode to string
-    return new_jwt.decode()
-
-
-# Load the private key from a file and decrypt if necessary
-def get_private_key(file_name, password=None):
-    try:
-        rsa_key = RSAKey.from_private_key_file(
-            GITHUB_SECRETS_PK_PEM_FILE, password)
-    except ssh_exception.PasswordRequiredException:
-        password = getpass(prompt="Private key password: ")
-        return get_private_key(file_name, password)
-    except ssh_exception.SSHException:
-        fatal('Invalid private key password')
-    except FileNotFoundError:
-        fatal('Could not find private key file')
-    with StringIO() as buf:
-        rsa_key.write_private_key(buf)
-        return buf.getvalue()
-
-
-def encrypt(public_key: str, secret_value: str) -> str:
-    """Encrypt a Unicode string using the public key."""
-    public_key = public.PublicKey(
-        public_key.encode("utf-8"), encoding.Base64Encoder())
-    sealed_box = public.SealedBox(public_key)
-    encrypted = sealed_box.encrypt(secret_value.encode("utf-8"))
-    return base64.b64encode(encrypted).decode("utf-8")
-
-
-def update_github_secret(
-        token_headers: dict, github_pub_key_JSON: dict,
-        file_to_be_encoded: str, secret_name: str, b64encode=True):
-    msg = ""
-    if b64encode:
-        base64_bytes = base64.b64encode(Path(file_to_be_encoded).read_bytes())
-        msg = base64_bytes.decode("utf-8")
-    else:
-        msg = Path(file_to_be_encoded).read_text()
-
-    secrets_url = 'https://api.github.com/orgs/ackersonde/actions/secrets'
-    encrypted_value = encrypt(github_pub_key_JSON['key'], msg)
-    r = requests.put(
-        f'{secrets_url}/{secret_name}',
-        json={"encrypted_value": f"{encrypted_value}",
-              "key_id": f"{github_pub_key_JSON['key_id']}",
-              "visibility": "all"},
-        headers=token_headers)
-    r.raise_for_status()
-
-
-def redeploy_digitalocean(access_token: str):
-    token_headers = {'Accept': 'application/vnd.github.v3+json',
-                     'Authorization': f'token {access_token}'}
-
+def redeploy_digitalocean(token_headers: str):
     resp = requests.post(
         'https://api.github.com/repos/ackersonde/digitaloceans/actions/workflows/build.yml/dispatches',
         json={"ref": "main"},
@@ -95,41 +22,32 @@ def redeploy_digitalocean(access_token: str):
 
 
 def main():
-    # Generate a new JWT using id and private key
-    pri_key = get_private_key(GITHUB_SECRETS_PK_PEM_FILE)
-    id_token = generate_id_token(GITHUB_APP_CLIENT_ID, pri_key)
-
     # https://docs.github.com/en/free-pro-team@latest/rest/reference/actions#secrets
     try:
-        url = f'https://api.github.com/app/installations/{GITHUB_INSTALL_ID}/access_tokens'
-        auth_headers = {'Accept': 'application/vnd.github.v3+json',
-                        'Authorization': f'Bearer {id_token}'}
-        resp = requests.post(url, headers=auth_headers)
-        resp.raise_for_status()
-        output = resp.json()
-        access_token = output['token']
+        token_headers = github.fetch_token_headers()
+        github_public_key = github.fetch_public_key(token_headers)
 
-        url = 'https://api.github.com/orgs/ackersonde/actions/secrets/public-key'
-        token_headers = {'Accept': 'application/vnd.github.v3+json',
-                         'Authorization': f'token {access_token}'}
-        resp = requests.get(url, headers=token_headers)
-        resp.raise_for_status()
-        github_pub_key_JSON = resp.json()
+        ssh_priv_key_args = SimpleNamespace(
+            name='CTX_SERVER_DEPLOY_SECRET_B64', base64=True, filepath=SSH_PRIV_KEY)
+        github.update_secret(token_headers, github_public_key, ssh_priv_key_args)
 
-        update_github_secret(token_headers, github_pub_key_JSON, SSH_PRIV_KEY,
-                             "CTX_SERVER_DEPLOY_SECRET_B64")
-        update_github_secret(token_headers, github_pub_key_JSON, SSH_CERT_FILE,
-                             "CTX_SERVER_DEPLOY_CACERT_B64")
-        update_github_secret(token_headers, github_pub_key_JSON, SSH_PUB_KEY,
-                             "CTX_SERVER_DEPLOY_PUBLIC_B64")
-        update_github_secret(token_headers, github_pub_key_JSON, ACME_JSON,
-                             "CTX_ACME_JSON", b64encode=False)
+        ssh_cert_file_args = SimpleNamespace(
+            name='CTX_SERVER_DEPLOY_CACERT_B64', base64=True, filepath=SSH_CERT_FILE)
+        github.update_secret(token_headers, github_public_key, ssh_cert_file_args)
 
-        redeploy_digitalocean(access_token)
+        ssh_pub_key_args = SimpleNamespace(
+            name='CTX_SERVER_DEPLOY_PUBLIC_B64', base64=True, filepath=SSH_PUB_KEY)
+        github.update_secret(token_headers, github_public_key, ssh_pub_key_args)
+
+        acme_json_args = SimpleNamespace(
+            name='CTX_ACME_JSON', base64=False, filepath=ACME_JSON)
+        github.update_secret(token_headers, github_public_key, acme_json_args)
+
+        redeploy_digitalocean(token_headers)
     except HTTPError as http_err:
-        fatal(f'HTTP error occurred: {http_err}')
+        github.fatal(f'HTTP error occurred: {http_err}')
     except Exception as err:
-        fatal(f'Other error occurred: {err}')
+        github.fatal(f'Other error occurred: {err}')
 
 
 if __name__ == '__main__':
