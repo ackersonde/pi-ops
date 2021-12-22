ebb2303e8697630149cf636b479b219074c47ce6
Dan Ackerson <dan@ackerson.de>
2021-12-22 17:09:00 +0100

    correct path for vault_data tarball

--- scripts/backup_restore.sh
+++ scripts/backup_restore.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 DATE=`date +"%Y.%m.%d.%Hh%Mm%Ss"`
+VAULT_HOME=/home/ubuntu/vault/vault_data
+BACKUP_HOST=192.168.178.28
+BACKUP_HOME=/mnt/usb4TB/backups/vault-secrets/
+MNT_PATH=/mnt/vault_data
 
 if ! [[ $# = 2 || $1 =~ ^(backup|restore)$ ]]
 then
@@ -9,20 +13,29 @@ else
     if [[ $1 = backup ]]
     then
       echo "Going to backup now..."
-      tar -czf backup-vault_data-$DATE.tgz -C /home/ubuntu/vault vault_data
-      scp backup-vault_data-$DATE.tgz 192.168.178.28:/mnt/usb4TB/backups/vault-secrets/
+      # local cleanup
+      docker rm -f vault || true
+      sudo rm -Rf $VAULT_HOME || true
+
+      # remote graceful shutdown and data sync to local disk
+      ssh -o StrictHostKeyChecking=no ackerson.de docker stop vault
+      rsync -Pav ackerson.de:$MNT_PATH . --delete
+
+      tar -czf backup-vault_data-$DATE.tgz -C $VAULT_HOME .
+      scp backup-vault_data-$DATE.tgz $BACKUP_HOST:$BACKUP_HOME
       rm backup-vault_data-$DATE.tgz
     elif [[ $2 ]]
     then
       FILE=${2/$BACKUP_DIR/} # if restore file contains backup path, remove it (copy/pasta)
       echo "Going to restore $FILE now..."
-      ssh ackerson.de docker stop vault
-      scp $BACKUP_HOST:$BACKUP_DIR/$FILE ackerson.de:$MNT_PATH
-      ssh ackerson.de tar -zxvf $MNT_PATH
-      docker start vault
+      exit 1 # not ready to test this yet!
+      ssh ackerson.de "docker stop vault && sudo rm $MNT_PATH/*"
+      scp 192.168.178.28:$BACKUP_HOME/$FILE .
+      scp $FILE ackerson.de:$MNT_PATH
+      ssh ackerson.de "cd $MNT_PATH; tar -zxvf $MNT_PATH/$FILE; rm $MNT_PATH/$FILE; docker start vault"
       rm $FILE
     else
       echo "Last 10 backups >15KB in size on vpnpi:"
-      ssh $BACKUP_HOST "find $BACKUP_DIR -type f -size +15k -printf '%T@ %p\n' -ls | sort -n | cut -d' ' -f 2 | tail -n 10 | xargs -r ls -l"
+      ssh $BACKUP_HOST "find $BACKUP_HOME -type f -size +10k -printf '%T@ %p\n' -ls | sort -n | cut -d' ' -f 2 | tail -n 10 | xargs -r ls -l"
     fi
 fi
