647760134bcc739a36d515da570a661dc2f4a20c
Dan Ackerson <dan@ackerson.de>
2022-02-12 17:07:46 +0100

    test behind traefik

--- scripts/secrets/test_vault_data.sh
+++ scripts/secrets/test_vault_data.sh
@@ -2,6 +2,13 @@
 # spin up local instance for testing sanity of data
 sudo chown -Rf systemd-network:ubuntu /home/ubuntu/vault/vault_data/*
 
-docker run -d --cap-add=IPC_LOCK -v /home/ubuntu/vault/vault_data:/vault \
-  -e VAULT_API_ADDR=http://127.0.0.1:8200 -e VAULT_ADDR=http://127.0.0.1:8200 \
-  -p 8200:8200 --name vault vault:1.9.1 server
+docker run -d --cap-add=IPC_LOCK \
+  -v /home/ubuntu/vault/vault_data:/vault \
+  -e VAULT_API_ADDR=http://127.0.0.1:8200 \
+  -e VAULT_ADDR=http://127.0.0.1:8200 \
+  --label='traefik.enable=true' \
+  --label='traefik.http.routers.vault.middlewares=http-ratelimit@file,secHeaders@file,api_auth' \
+  --label='traefik.http.routers.vault.tls.certResolver=letsencrypt' \
+  --label='traefik.http.routers.vault.tls.domains=hv.ackerson.de' \
+  --label='traefik.http.routers.vault.rule=Host(\`hv.ackerson.de\`)' \
+  --name vault vault:latest server
