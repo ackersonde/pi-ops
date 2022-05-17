c57f0e0ab82ceabc33fff2b67db95d9a72022a23
danackerson <dan@ackerson.de>
2022-05-17 16:40:33 +0200

    migrate to hvac lib with AppRoles; use black

--- scripts/secrets/github.py
+++ scripts/secrets/github.py
@@ -12,47 +12,53 @@ import os
 import requests
 import sys
 
-GITHUB_APP_CLIENT_ID = os.environ['GITHUB_APP_CLIENT_ID']
-GITHUB_INSTALL_ID = os.environ['GITHUB_INSTALL_ID']
-GITHUB_SECRETS_PK_PEM = os.environ['GITHUB_SECRETS_PK_PEM']
+GITHUB_APP_CLIENT_ID = os.environ["GITHUB_APP_CLIENT_ID"]
+GITHUB_INSTALL_ID = os.environ["GITHUB_INSTALL_ID"]
+GITHUB_SECRETS_PK_PEM = os.environ["GITHUB_SECRETS_PK_PEM"]
 
 
 def fatal(message):
-    print('Error: {}'.format(message), file=sys.stderr)
+    print("Error: {}".format(message), file=sys.stderr)
     sys.exit(1)
+
 
 def get_updated_secrets_metadata():
     github_secrets = {}
     token_headers = fetch_token_headers()
 
-    secrets_url = 'https://api.github.com/orgs/ackersonde/actions/secrets?per_page=100'
+    secrets_url = "https://api.github.com/orgs/ackersonde/actions/secrets?per_page=100"
     try:
-      r = requests.get(secrets_url, headers=token_headers)
-      r.raise_for_status()
+        r = requests.get(secrets_url, headers=token_headers)
+        r.raise_for_status()
 
-      j = r.json()
-      for secret in j['secrets']:
-        github_secrets[secret['name']] = secret['updated_at']
+        j = r.json()
+        for secret in j["secrets"]:
+            github_secrets[secret["name"]] = secret["updated_at"]
     except HTTPError as http_err:
-      fatal(f'HTTP error occurred while fetching metadata: {http_err}')
+        fatal(f"HTTP error occurred while fetching metadata: {http_err}")
     except Exception as err:
-      fatal(f'Other error occurred while fetching metadata: {err}')
+        fatal(f"Other error occurred while fetching metadata: {err}")
 
     return github_secrets
 
+
 # Token Exchange requires a JWT in the Auth Bearer header with this format
 def generate_id_token(iss, expire_seconds=600):
-    #raw_pem_key = open(GITHUB_SECRETS_PK_PEM_FILE, "r").read()
-    signing_key = serialization.load_pem_private_key(GITHUB_SECRETS_PK_PEM.encode(), password=None)
+    # raw_pem_key = open(GITHUB_SECRETS_PK_PEM_FILE, "r").read()
+    signing_key = serialization.load_pem_private_key(
+        GITHUB_SECRETS_PK_PEM.encode(), password=None
+    )
 
     token = jwt.encode(
-        {'iss': iss, 'iat': int(time()), 'exp': int(time()) + expire_seconds},
-        signing_key, algorithm='RS256')
+        {"iss": iss, "iat": int(time()), "exp": int(time()) + expire_seconds},
+        signing_key,
+        algorithm="RS256",
+    )
 
     key = RSA.import_key(GITHUB_SECRETS_PK_PEM)
-    decoded = jwt.decode(token, key.public_key().export_key(), algorithms=['RS256'])
-    if decoded['iss'] != iss:
-        raise ValueError('Invalid token')
+    decoded = jwt.decode(token, key.public_key().export_key(), algorithms=["RS256"])
+    if decoded["iss"] != iss:
+        raise ValueError("Invalid token")
 
     return token
 
@@ -73,50 +79,59 @@ def update_secret(token_headers: dict, github_JSON: dict, args: SimpleNamespace)
 
     payload = ""
     if args.filepath:
-      file = Path(args.filepath)
-      if b64encode:
-        base64_bytes = base64.b64encode(file.read_bytes())
-        payload = base64_bytes.decode("utf-8")
-      else:
-        payload = file.read_text()
+        file = Path(args.filepath)
+        if b64encode:
+            base64_bytes = base64.b64encode(file.read_bytes())
+            payload = base64_bytes.decode("utf-8")
+        else:
+            payload = file.read_text()
     else:
-      payload = args.value
-      if b64encode:
-        base64_bytes = base64.b64encode(bytes(payload, "utf-8"))
-        payload = base64_bytes.decode("utf-8")
+        payload = args.value
+        if b64encode:
+            base64_bytes = base64.b64encode(bytes(payload, "utf-8"))
+            payload = base64_bytes.decode("utf-8")
 
-    secrets_url = 'https://api.github.com/orgs/ackersonde/actions/secrets'
-    encrypted_value = encrypt(github_JSON['key'], payload)
+    secrets_url = "https://api.github.com/orgs/ackersonde/actions/secrets"
+    encrypted_value = encrypt(github_JSON["key"], payload)
 
     try:
-      r = requests.put(
-        f'{secrets_url}/{secret_name}',
-        json={"encrypted_value": f"{encrypted_value}",
-              "key_id": f"{github_JSON['key_id']}",
-              "visibility": "all"},
-        headers=token_headers)
-      r.raise_for_status()
+        r = requests.put(
+            f"{secrets_url}/{secret_name}",
+            json={
+                "encrypted_value": f"{encrypted_value}",
+                "key_id": f"{github_JSON['key_id']}",
+                "visibility": "all",
+            },
+            headers=token_headers,
+        )
+        r.raise_for_status()
     except HTTPError as http_err:
-      fatal(f'HTTP error occurred during secret update: {http_err}')
+        fatal(f"HTTP error occurred during secret update: {http_err}")
     except Exception as err:
-      fatal(f'Other error occurred during secret update: {err}')
+        fatal(f"Other error occurred during secret update: {err}")
+
 
 def fetch_token_headers():
     id_token = generate_id_token(iss=GITHUB_APP_CLIENT_ID)
 
-    url = f'https://api.github.com/app/installations/{GITHUB_INSTALL_ID}/access_tokens'
-    auth_headers = {'Accept': 'application/vnd.github.v3+json',
-                    'Authorization': f'Bearer {id_token}'}
+    url = f"https://api.github.com/app/installations/{GITHUB_INSTALL_ID}/access_tokens"
+    auth_headers = {
+        "Accept": "application/vnd.github.v3+json",
+        "Authorization": f"Bearer {id_token}",
+    }
     resp = requests.post(url, headers=auth_headers)
     resp.raise_for_status()
     output = resp.json()
 
-    token = output['token']
-    return {'Accept': 'application/vnd.github.v3+json',
-            'Authorization': f'token {token}'}
+    token = output["token"]
+    return {
+        "Accept": "application/vnd.github.v3+json",
+        "Authorization": f"token {token}",
+    }
+
 
 def fetch_public_key(token_headers: str):
-    url = 'https://api.github.com/orgs/ackersonde/actions/secrets/public-key'
+    url = "https://api.github.com/orgs/ackersonde/actions/secrets/public-key"
 
     resp = requests.get(url, headers=token_headers)
     resp.raise_for_status()
--- scripts/secrets/vault_update_secrets.py
+++ scripts/secrets/vault_update_secrets.py
@@ -2,198 +2,195 @@ from datetime import datetime
 from types import SimpleNamespace
 
 import github
+import hvac
 import os
 import json
 import re
 import requests
 
-BASICAUTH_USER = os.environ['BASICAUTH_USER']
-BASICAUTH_PASS = os.environ['BASICAUTH_PASS']
-VAULT_API_ENDPOINT = os.environ['VAULT_API_ENDPOINT']
-VAULT_READ_USER = os.environ['VAULT_READ_USER']
-VAULT_READ_PASS = os.environ['VAULT_READ_PASS']
-VAULT_READ_TOKEN = os.environ['VAULT_READ_TOKEN']
-VAULT_WRITE_USER = os.environ['VAULT_WRITE_USER']
-VAULT_WRITE_PASS = os.environ['VAULT_WRITE_PASS']
-VAULT_WRITE_TOKEN = os.environ['VAULT_WRITE_TOKEN']
+VAULT_API_ENDPOINT = os.environ["VAULT_API_ENDPOINT"]
+VAULT_READ_APPROLE_ID = os.environ["VAULT_READ_APPROLE_ID"]
+VAULT_READ_SECRET_ID = os.environ["VAULT_READ_SECRET_ID"]
+VAULT_READ_TOKEN = os.environ["VAULT_READ_TOKEN"]
+VAULT_WRITE_APPROLE_ID = os.environ["VAULT_WRITE_APPROLE_ID"]
+VAULT_WRITE_SECRET_ID = os.environ["VAULT_WRITE_SECRET_ID"]
+VAULT_WRITE_TOKEN = os.environ["VAULT_WRITE_TOKEN"]
 
 
-def get_vault_token(auth, headers, readonly=True):
+# https://hvac.readthedocs.io/en/stable/usage/auth_methods/approle.html
+def get_vault_token(readonly=True) -> str:
     exception_method = "get_vault_token(): "
-    vault_user = VAULT_READ_USER
-    vault_pass = VAULT_READ_PASS
+    vault_secret = VAULT_READ_SECRET_ID
+    vault_role = VAULT_READ_APPROLE_ID
     token = ""
     if not readonly:
-        vault_user = VAULT_WRITE_USER
-        vault_pass = VAULT_WRITE_PASS
+        vault_role = VAULT_WRITE_APPROLE_ID
+        vault_secret = VAULT_WRITE_SECRET_ID
 
     try:
-        url = VAULT_API_ENDPOINT + '/v1/auth/robots/login/' + vault_user
-        data = '{"password": "' + vault_pass + '", "ttl": "1h" }'
-
-        r = requests.post(url, auth=auth, headers=headers, data=data)
-        r.raise_for_status()
-
-        j = r.json()
-        token = j['auth']['client_token']
-    except json.decoder.JSONDecodeError as json_err:
-        print(exception_method + f'JSON parsing error occurred: {json_err}')
+        client = hvac.Client(url=VAULT_API_ENDPOINT)
+        r = client.auth.approle.login(
+            role_id=vault_role,
+            secret_id=vault_secret,
+        )
+        # res = client.is_authenticated()
+        print("res1:", r)
+        token = r["auth"]["client_token"]
     except requests.exceptions.HTTPError as http_err:
-        print(exception_method + f'HTTP error occurred: {http_err}')  # Python 3.6
+        print(exception_method + f"HTTP error occurred: {http_err}")  # Python 3.6
     except Exception as err:
-        print(exception_method + f'Other error occurred: {err}')  # Python 3.6
+        print(exception_method + f"Other error occurred: {err}")  # Python 3.6
 
     return token
 
 
-def get_updated_secrets_metadata(auth, headers, token):
-    exception_method = "get_updated_secrets(): "
+def get_updated_secrets_metadata(client):
+    exception_method = "get_updated_secrets_metadata(): "
     vault_secrets = {}
 
     try:
+        # https://hvac.readthedocs.io/en/stable/usage/secrets_engines/kv_v2.html#read-secret-metadata
         # First LIST all secrets at secret engine "github/ackersonde"
-        url = VAULT_API_ENDPOINT + "/v1/github%2Fackersonde/metadata/?list=1"
-        headers["X-Vault-Token"] = token
-
-        r = requests.get(url, auth=auth, headers=headers)
-        r.raise_for_status()
-
-        j = r.json()
-        secret_names = j['data']['keys']
+        r = client.secrets.kv.v2.list_secrets(path="", mount_point="github/ackersonde")
+        secret_names = r["data"]["keys"]
 
         # Second: LOOP thru each secret get last timestamp
         for secret_name in secret_names:
-          url = VAULT_API_ENDPOINT + "/v1/github%2Fackersonde/metadata/" + secret_name
-
-          r = requests.get(url, auth=auth, headers=headers)
-          r.raise_for_status()
-
-          j = r.json()
-          vault_secrets[secret_name] = j['data']['updated_time']
-    except json.decoder.JSONDecodeError as json_err:
-        print(exception_method + f'JSON parsing error occurred: {json_err}')
+            r = client.secrets.kv.v2.read_secret_metadata(
+                mount_point="github/ackersonde", path=secret_name
+            )
+            vault_secrets[secret_name] = r["data"]["updated_time"]
     except requests.exceptions.HTTPError as http_err:
-        print(exception_method + f'HTTP error occurred: {http_err}')  # Python 3.6
+        print(exception_method + f"HTTP error occurred: {http_err}")  # Python 3.6
     except Exception as err:
-        print(exception_method + f'Other error occurred: {err}')  # Python 3.6
+        print(exception_method + f"Other error occurred: {err}")  # Python 3.6
 
     return vault_secrets
 
 
 def main():
-    auth = requests.auth.HTTPBasicAuth(BASICAUTH_USER, BASICAUTH_PASS)
-
-    headers = requests.structures.CaseInsensitiveDict()
-    headers["Content-Type"] = "application/json"
-
     read_token = VAULT_READ_TOKEN
     if read_token == "":
-        read_token = get_vault_token(auth, headers, readonly = True)
-        print(f'READ: {read_token}')
+        read_token = get_vault_token()
+        # print(f"READ1: {read_token}"
 
     if read_token:
-        vault_secrets = get_updated_secrets_metadata(auth, headers, read_token)
+        client = hvac.Client(url=VAULT_API_ENDPOINT, token=read_token)
+        if not client.is_authenticated():
+            read_token = get_vault_token()
+            client = hvac.Client(url=VAULT_API_ENDPOINT, token=read_token)
+
+        vault_secrets = get_updated_secrets_metadata(client)
         # print(vault_secrets)
 
-        # print('=================================================================')
+        # print("=================================================================")
 
         # fetch the last updated time of the secrets on github
         github_secrets = github.get_updated_secrets_metadata()
         # print(len(github_secrets))
 
-        update_github_secrets(vault_secrets, github_secrets, auth, headers, read_token)
+        update_github_secrets(vault_secrets, github_secrets, client)
     else:
-        print("Unable to get Vault token - cowardly refusing to fetch updated secrets...")
+        print(
+            "Unable to get Vault token - cowardly refusing to fetch updated secrets..."
+        )
 
 
-def update_github_secrets(vault_secrets, github_secrets, auth, headers, read_token):
+def update_github_secrets(vault_secrets, github_secrets, client):
     token_headers = github.fetch_token_headers()
     github_public_key = github.fetch_public_key(token_headers)
 
     # update the secrets which are out of sync on github
     for secret_name in vault_secrets.keys():
         slack_update = ""
+        vault_updated_at = re.sub(r"\..*?Z", "", vault_secrets[secret_name]) + "+00:00"
+        vault_update_time = datetime.fromisoformat(vault_updated_at)
 
         if secret_name in github_secrets:
             github_updated_at = github_secrets[secret_name].replace("Z", "+00:00")
             github_update_time = datetime.fromisoformat(github_updated_at)
 
-            vault_updated_at = re.sub("\..*?Z", "", vault_secrets[secret_name]) + "+00:00"
-            vault_update_time = datetime.fromisoformat(vault_updated_at)
-
-            #print(f'{secret_name} needs updating at Github: {github_update_time < vault_update_time}')
+            # print(f'{secret_name} needs updating at Github: {github_update_time < vault_update_time}')
 
             if github_update_time < vault_update_time:
-                secret = get_secret_value(auth, headers, read_token, secret_name)
+                secret = get_secret_value(client, secret_name)
                 for secret_name, value in secret.items():
-                    json_args = SimpleNamespace(name=secret_name, base64=True, value=value, filepath='')
+                    json_args = SimpleNamespace(
+                        name=secret_name, base64=True, value=value, filepath=""
+                    )
                     github.update_secret(token_headers, github_public_key, json_args)
-                    slack_update = f'Updated github w/ *{secret_name}*.'
+                    slack_update = f"Updated github secret *{secret_name}* (changed in Vault on {vault_update_time})."
         else:
-            secret = get_secret_value(auth, headers, read_token, secret_name)
+            secret = get_secret_value(client, secret_name)
             for secret_name, value in secret.items():
-                json_args = SimpleNamespace(name=secret_name, base64=True, value=value, filepath='')
+                json_args = SimpleNamespace(
+                    name=secret_name, base64=True, value=value, filepath=""
+                )
                 github.update_secret(token_headers, github_public_key, json_args)
-                slack_update = f'Created *{secret_name}* @ github.'
+                slack_update = f"Created *{secret_name}* @ github (added to Vault on {vault_update_time}."
 
         if slack_update:
-            update_slack(slack_update, secret_name)
+            notify_slack(slack_update, secret_name)
 
 
-def update_slack(slack_update, secret_name):
+def notify_slack(slack_update, secret_name):
     exception_method = "update_slack():"
     impacted_repo_links = ""
 
     try:
         headers = {}
-        url = f'https://api.github.com/search/code?q=org%3Aackersonde+{secret_name}&type=Code'
-        headers['Accept'] = "application/vnd.github.v3+json"
+        url = f"https://api.github.com/search/code?q=org%3Aackersonde+{secret_name}&type=Code"
+        headers["Accept"] = "application/vnd.github.v3+json"
         r = requests.get(url, headers=headers)
         r.raise_for_status()
 
         j = r.json()
-        for repository in j['items']:
+        for repository in j["items"]:
             impacted_repo_links += f"<{repository['repository']['html_url']} | {repository['repository']['name']}>, "
 
         if impacted_repo_links:
-            impacted_repo_links = " Following repos are effected and should be redeployed: " + impacted_repo_links.removesuffix(", ")
+            impacted_repo_links = (
+                " Following repos are effected and should be redeployed: "
+                + impacted_repo_links.removesuffix(", ")
+            )
             slack_update += impacted_repo_links
 
         print(slack_update)
-        r = requests.post('https://slack.com/api/chat.postMessage', {
-            'token': os.environ['SLACK_API_TOKEN'],
-            'channel': 'C092UE0H4',
-            'text': slack_update
-            })
+        r = requests.post(
+            "https://slack.com/api/chat.postMessage",
+            {
+                "token": os.environ["SLACK_API_TOKEN"],
+                "channel": "C092UE0H4",
+                "text": slack_update,
+            },
+        )
         r.raise_for_status()
     except json.decoder.JSONDecodeError as json_err:
-        print(exception_method + f'JSON parsing error occurred: {json_err}')
+        print(exception_method + f"JSON parsing error occurred: {json_err}")
     except requests.exceptions.HTTPError as http_err:
-        print(exception_method + f'HTTP error occurred: {http_err}')  # Python 3.6
+        print(exception_method + f"HTTP error occurred: {http_err}")  # Python 3.6
     except Exception as err:
-        print(exception_method + f'Other error occurred: {err}')  # Python 3.6
+        print(exception_method + f"Other error occurred: {err}")  # Python 3.6
 
 
-def get_secret_value(auth, headers, token, secret_name):
+def get_secret_value(client, secret_name):
     exception_method = "get_secret_value(): "
     response = {}
 
     try:
-        headers["X-Vault-Token"] = token
-        url = VAULT_API_ENDPOINT + "/v1/github%2Fackersonde/data/" + secret_name
-        r = requests.get(url, auth=auth, headers=headers)
-        r.raise_for_status()
-        j = r.json()
-        response[secret_name] = j['data']['data'][secret_name]
-    except json.decoder.JSONDecodeError as json_err:
-        print(exception_method + f'JSON parsing error occurred: {json_err}')
+        # https://hvac.readthedocs.io/en/stable/usage/secrets_engines/kv_v2.html#read-secret-versions
+        r = client.secrets.kv.v2.read_secret_version(
+            mount_point="github/ackersonde",
+            path=secret_name,
+        )
+        response[secret_name] = r["data"]["data"][secret_name]
     except requests.exceptions.HTTPError as http_err:
-        print(exception_method + f'HTTP error occurred: {http_err}')  # Python 3.6
+        print(exception_method + f"HTTP error occurred: {http_err}")  # Python 3.6
     except Exception as err:
-        print(exception_method + f'Other error occurred: {err}')  # Python 3.6
+        print(exception_method + f"Other error occurred: {err}")  # Python 3.6
 
     return response
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
