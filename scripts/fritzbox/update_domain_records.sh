972642cfcd73cb2777c74a181e787d14a8bd6d9e
danackerson <dan@ackerson.de>
2022-07-09 17:10:21 +0200

    fix and use domain names

--- scripts/fritzbox/update_domain_records.sh
+++ scripts/fritzbox/update_domain_records.sh
@@ -7,14 +7,14 @@ PREFIX=`./fritzBoxShell.sh IGDIP STATE | grep NewIPv6Prefix | awk '{print $2}'`
 sudo service systemd-resolved restart
 
 declare -A domains
-domains["145482932"]="ackersond@{{IPV6_BUILD_HOME}}"
-domains["145482150"]="ubuntu@{{IPV6_MASTER_HOME}}"
-domains["145483151"]="ubuntu@{{IPV6_SLAVE_HOME}}"
+domains["145482932"]="ackersond@{{BUILD_HOME}}"
+domains["145482150"]="ubuntu@{{MASTER_HOME}}"
+domains["145483151"]="ubuntu@{{SLAVE_HOME}}"
 
-for IPV6_RECORD_ID in "${!domains[@]}"
+for DO_IPV6_RECORD_ID in "${!domains[@]}"
 do
-    local_ip=`ssh ${domains[$IPV6_RECORD_ID]} curl --silent https://ipv6.icanhazip.com/ | xargs echo -n`
-    domain=`echo ${domains[$IPV6_RECORD_ID]} | awk -F '@' '{print $2}'`
+    local_ip=`ssh ${domains[$DO_IPV6_RECORD_ID]} curl --silent https://ipv6.icanhazip.com/ | xargs echo -n`
+    domain=`echo ${domains[$DO_IPV6_RECORD_ID]} | awk -F '@' '{print $2}'`
     dns_look=`dig +short AAAA $domain`
 
     echo "$domain - iCAN: $local_ip  DIG: $dns_look"
@@ -23,9 +23,9 @@ do
     if [[ "$local_ip" != "$dns_look" ]]; then
         # if not, check if IPv6 prefix has changed
         if [[ ! $local_ip =~ ^"${PREFIX::-1}".* ]]; then
-            echo "UPDATE ${domains[$IPV6_RECORD_ID]} to $local_ip !"
+            echo "UPDATE ${domains[$DO_IPV6_RECORD_ID]} to $local_ip !"
             # curl --silent -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer $DO_TOKEN" \
-            #    -d "{\"data\":\"$local_ip\"}" "https://api.digitalocean.com/v2/domains/$DOMAIN/records/$IPV6_RECORD_ID"
+            #    -d "{\"data\":\"$local_ip\"}" "https://api.digitalocean.com/v2/domains/$DOMAIN/records/$DO_IPV6_RECORD_ID"
 
             # send Slack update msg
             curl -s -o /dev/null -X POST -d token=$SLACK_API_TOKEN -d channel=C092UE0H4 \
