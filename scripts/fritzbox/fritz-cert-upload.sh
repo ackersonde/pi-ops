19646595425d3cd5271667325af154a4ed46de43
Dan Ackerson <dan@ackerson.de>
2022-09-02 09:21:50 +0200

    finish migration to certbot

--- scripts/fritzbox/fritz-cert-upload.sh
+++ scripts/fritzbox/fritz-cert-upload.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
-## thanks to https://gist.github.com/mahowi for the perfect Idea
-## put it in /etc/letsencrypt/renewal-hooks/post so it gets run after every renewal.
 
 # parameters
 USERNAME="{{FRITZ_BOX_USER}}"
 PASSWORD="{{FRITZ_BOX_PASS}}"
-CERTPATH="/home/ubuntu/traefik/dump"
+CERTPATH="/etc/letsencrypt/live/ackerson.de"
 HOST=https://fritz.ackerson.de
 WGET="wget" # add `--no-check-certificate` if/when you've missed the expiration
 
 # make and secure a temporary file
 TMP="$(mktemp -t XXXXXX)"
 chmod 600 $TMP
-
-/usr/bin/scp -o StrictHostKeyChecking=no -i /home/ubuntu/my-ca/id_ed25519_github_deploy \
-    root@ackerson.de:/root/traefik/acme.json /home/ubuntu/traefik/acme.json
-/usr/bin/chmod 600 /home/ubuntu/traefik/acme.json
-# parse out certificates from Traefik 2.2 acme.json file
-# can be found @ https://github.com/ldez/traefik-certs-dumper
-/home/ubuntu/traefik/traefik-certs-dumper file --version v2 \
-    --source /home/ubuntu/traefik/acme.json \
-    --dest /home/ubuntu/traefik/dump
 
 # login to the box and get a valid SID
 CHALLENGE=`$WGET -q -O - $HOST/login_sid.lua | sed -e 's/^.*<Challenge>//' -e 's/<\/Challenge>.*$//'`
@@ -34,8 +23,8 @@ printf "Content-Disposition: form-data; name=\"sid\"\r\n\r\n$SID\r\n" >> $TMP
 printf -- "--$BOUNDARY\r\n" >> $TMP
 printf "Content-Disposition: form-data; name=\"BoxCertImportFile\"; filename=\"BoxCert.pem\"\r\n" >> $TMP
 printf "Content-Type: application/octet-stream\r\n\r\n" >> $TMP
-cat $CERTPATH/private/\*.ackerson.de.key >> $TMP
-cat $CERTPATH/certs/\*.ackerson.de.crt >> $TMP
+cat $CERTPATH/privkey.pem >> $TMP
+cat $CERTPATH/fullchain.pem >> $TMP
 printf "\r\n" >> $TMP
 printf -- "--$BOUNDARY--" >> $TMP
 
@@ -44,4 +33,3 @@ $WGET -O - $HOST/cgi-bin/firmwarecfg --header="Content-type: multipart/form-data
 
 # clean up
 rm -f $TMP
-#rm -Rf /home/ubuntu/traefik/dump/
