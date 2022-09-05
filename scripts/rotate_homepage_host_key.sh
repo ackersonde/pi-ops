4a6396dd1edeaaa57503ef7ac7a0294489fdfbac
Dan Ackerson <dan@ackerson.de>
2022-09-05 14:52:54 +0200

    test complete

--- scripts/rotate_homepage_host_key.sh
+++ scripts/rotate_homepage_host_key.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [[ $(< ~/my-ca/homepage_host_key) != "$(ssh-keyscan -t ed25519 ackerson.de | awk '{print $3}')" ]]; then
+if [[ $(< ~/my-ca/homepage_host_key) == "$(ssh-keyscan -t ed25519 ackerson.de | awk '{print $3}')" ]]; then
   ssh-keygen -R ackerson.de && ssh-keyscan -H ackerson.de >> ~/.ssh/known_hosts
 else
   echo "$(date) -- SSH host key for ackerson.de does NOT match"
